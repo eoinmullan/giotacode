aa4a838cb9dedefeb89e11c18fab8e158e45a559
Eoin Mullan <eoinmullan@hotmail.com>
2014-05-14 21:47:09 +0100

    Moving InternalsVisibleTo attribute to AssemblyInfo.cs file to keep the unit test a bit cleaner

--- C#/Decryption/Models/EncryptedText.cs
+++ C#/Decryption/Models/EncryptedText.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 using Decryption.Interfaces;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("DecryptionUnitTests")]
-
 namespace Decryption.Models {
     internal class EncryptedText : IEncryptedText {
         public string Text { get; set; }
