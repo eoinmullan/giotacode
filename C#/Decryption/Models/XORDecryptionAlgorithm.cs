4f1e17fb3117bf9dcb3fcdb5130cf6ec0e4431e1
Eoin Mullan <eoinmullan@hotmail.com>
2014-05-14 21:47:49 +0100

    Adding XOR decryption algorithm.

---
+++ C#/Decryption/Models/XORDecryptionAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Decryption.Interfaces;
+
+namespace Decryption.Models {
+    internal class XORDecryptionAlgorithm : IDecryptionAlgorithm {
+        public byte[] Key { get; set; }
+
+        public XORDecryptionAlgorithm() : this(0) {
+        }
+
+        public XORDecryptionAlgorithm(params byte[] key) {
+            Key = key;
+        }
+
+        public string DecryptText(string encryptedText) {
+            return String.Concat(encryptedText.Split(',').Select((x, i) => (char)(Int32.Parse(x) ^ Key[i % Key.Length])));
+        }
+    }
+}
---
+++ C#/DecryptionUnitTests/XORDecryptionAlgorithmUnitTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Decryption.Models;
+using System.Linq;
+using System.Diagnostics;
+
+namespace DecryptionUnitTests {
+    [TestClass]
+    public class XORDecryptionAlgorithmUnitTest {
+        private XORDecryptionAlgorithm target;
+
+        [TestInitialize]
+        public void Initialize() {
+            target = new XORDecryptionAlgorithm();
+        }
+
+        [TestMethod]
+        public void ShouldConvertASCIICodesToCharactersWhenKeyIsNotSet() {
+            var upperCaseASCIICodes = Enumerable.Range(65, 26);
+            var upperCaseAlphabet = String.Concat(upperCaseASCIICodes.Select(x => (char)x));
+            var commaSeperatedUpperCaseASCIICodes = string.Join(",", upperCaseASCIICodes);
+            Assert.AreEqual(upperCaseAlphabet, target.DecryptText(commaSeperatedUpperCaseASCIICodes));
+
+            var lowerCaseASCIICodes = Enumerable.Range(65, 26);
+            var lowerCaseAlphabet = String.Concat(lowerCaseASCIICodes.Select(x => (char)x));
+            var commaSeperatedLowerCaseASCIICodes = string.Join(",", lowerCaseASCIICodes);
+            Assert.AreEqual(lowerCaseAlphabet, target.DecryptText(commaSeperatedLowerCaseASCIICodes));
+        }
+
+        [TestMethod]
+        public void ShouldConvert65TokWhenKeyIs42() {
+            target.Key = new byte[] {42};
+
+            Assert.AreEqual("kkkkk", target.DecryptText("65,65,65,65,65"));
+        }
+
+        [TestMethod]
+        public void ShouldConvert107ToAWhenKeyIs42() {
+            target.Key = new byte[] { 42 };
+
+            Assert.AreEqual("AAAAA", target.DecryptText("107,107,107,107,107"));
+        }
+
+        [TestMethod]
+        public void ShouldRepeatKeyCyclicallyThroughoutMessage() {
+            target.Key = new byte[] { 23, 32, 42, 200, 219 };
+
+            Assert.AreEqual("Secret Message", target.DecryptText("68,69,73,186,190,99,0,103,173,168,100,65,77,173"));
+        }
+
+        [TestMethod]
+        public void ShouldUseKeySuppliedInConstructor() {
+            var newTarget = new XORDecryptionAlgorithm(new byte[] { 23, 32, 42, 200, 219 });
+
+            Assert.AreEqual("Secret Message", newTarget.DecryptText("68,69,73,186,190,99,0,103,173,168,100,65,77,173"));
+        }
+    }
+}
