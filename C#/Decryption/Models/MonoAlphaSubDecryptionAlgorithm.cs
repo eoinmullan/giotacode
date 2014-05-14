87b46a0a2ce79fa40de120fedd38bf6cc7626234
Eoin Mullan <eoinmullan@hotmail.com>
2014-05-14 23:42:25 +0100

    Adding mono alphabetic substitution class.

---
+++ C#/Decryption/Models/MonoAlphaSubDecryptionAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Decryption.Interfaces;
+
+namespace Decryption.Models {
+    internal class MonoAlphaSubDecryptionAlgorithm : IDecryptionAlgorithm {
+        IDictionary<char, char> pairs;
+
+        public MonoAlphaSubDecryptionAlgorithm() {
+            pairs = new Dictionary<char, char>();
+        }
+
+        public void LoadSubstitutionPair(char find, char replace) {
+            if (pairs.ContainsKey(find)) {
+                pairs[find] = replace;
+            }
+            else {
+                pairs.Add(find, replace);
+            }
+        }
+
+        public string DecryptText(string encryptedText) {
+            return String.Concat(encryptedText.Select(x => ReplaceOrQuestionMark(x)));
+        }
+
+        private char ReplaceOrQuestionMark(char c) {
+            if (pairs.ContainsKey(c)) {
+                return pairs[c];
+            }
+
+            return '?';
+        }
+    }
+}
---
+++ C#/Decryption/UnitTests/MonoAlphaSubDecryptionAlgorithmUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Decryption.Models;
+
+namespace DecryptionUnitTests {
+    [TestClass]
+    public class MonoAlphaSubDecryptionAlgorithmUnitTest {
+        private MonoAlphaSubDecryptionAlgorithm target;
+
+        [TestInitialize]
+        public void Initialize() {
+            target = new MonoAlphaSubDecryptionAlgorithm();
+        }
+
+        [TestMethod]
+        public void ShouldReturnAllQuestionMarksWhenNoKeyIsSet() {
+            Assert.AreEqual("??????????", target.DecryptText("a12BC4./9y"));
+        }
+
+        [TestMethod]
+        public void ShouldReplaceCharactersThatHaveBeenSpecified() {
+            target.LoadSubstitutionPair('A', 'Q');
+            target.LoadSubstitutionPair('e', 'u');
+            target.LoadSubstitutionPair('.', '.');
+
+            Assert.AreEqual("Qu?.??", target.DecryptText("Aes.Mu"));
+        }
+
+        [TestMethod]
+        public void ShouldAllowSubstitutionPairsToBeReplaced() {
+            target.LoadSubstitutionPair('A', 'Q');
+            target.LoadSubstitutionPair('e', 'G');
+            target.LoadSubstitutionPair('e', 'u');
+
+            Assert.AreEqual("Qu????", target.DecryptText("Aes.Mu"));
+        }
+
+        [TestMethod]
+        public void ShouldNotReplaceCharactersThatHaveJustBeenSubstitutedIn() {
+            target.LoadSubstitutionPair('a', 'b');
+            target.LoadSubstitutionPair('b', 'c');
+
+            Assert.AreEqual("bc?", target.DecryptText("abc"));
+            Assert.AreEqual("?cb", target.DecryptText("cba"));
+        }
+    }
+}
