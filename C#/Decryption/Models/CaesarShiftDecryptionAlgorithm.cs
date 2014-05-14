126a1fd3faaae415411c5f893640b5f8f26b06fe
Eoin Mullan <eoinmullan@hotmail.com>
2014-05-14 23:05:59 +0100

    Adding Caesar shift decryption algorithm

---
+++ C#/Decryption/Models/CaesarShiftDecryptionAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Decryption.Interfaces;
+
+namespace Decryption.Models {
+    internal class CaesarShiftDecryptionAlgorithm : IDecryptionAlgorithm {
+        public int Key { get; set; }
+
+        public CaesarShiftDecryptionAlgorithm() : this(0) { 
+        }
+
+        public CaesarShiftDecryptionAlgorithm(int key) {
+            Key = key;
+        }
+
+        public string DecryptText(string encryptedText) {
+            return String.Concat(encryptedText.Select(x => ShiftCharacter(x)));
+        }
+
+        private char ShiftCharacter(char character) {
+            if (char.IsUpper(character)) {
+                return (char)(((character - 'A' + Key) % 26) + 'A');
+            }
+            else if (char.IsLower(character)) {
+                return (char)(((character - 'a' + Key) % 26) + 'a');
+            }
+
+            return character;
+        }
+    }
+}
---
+++ C#/DecryptionUnitTests/CaesarShiftDecryptionAlgorithmUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Decryption.Models;
+
+namespace DecryptionUnitTests {
+    [TestClass]
+    public class CaesarShiftDecryptionAlgorithmUnitTest {
+        private CaesarShiftDecryptionAlgorithm target;
+
+        [TestInitialize]
+        public void Initialize() {
+            target = new CaesarShiftDecryptionAlgorithm();
+        }
+
+        [TestMethod]
+        public void ShouldReturnEncryptedTextUnchangedWhenNoKeyIsSet() {
+            var testString = "The Quick Brown Fox Jumped Over The Lazy Dog.";
+
+            Assert.AreEqual(testString, target.DecryptText(testString));
+        }
+
+        [TestMethod]
+        public void ShouldShiftBy16CharactersWordsRespectingCase() {
+            target.Key = 16;
+            var encryptedWords = "AesMu LbYgx PyH TeWZc yFob dro VkJI Nyq".Split(' ');
+            var decryptedWords = "QuiCk BrOwn FoX JuMPs oVer the LaZY Dog".Split(' ');
+
+            for (var i = 0; i < encryptedWords.Length; i++) {
+                Assert.AreEqual(decryptedWords[i], target.DecryptText(encryptedWords[i]));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldShiftMessageWhileLeavingPuntuationIntact() {
+            target.Key = 16;
+            var encryptedSentence = "Aes.Mu L?bYgx P<yH> Te(WZc) yFob! d££ro V;k:JI N]y[q";
+            var decryptedSentence = "Qui.Ck B?rOwn F<oX> Ju(MPs) oVer! t££he L;a:ZY D]o[g";
+
+            Assert.AreEqual(decryptedSentence, target.DecryptText(encryptedSentence));
+        }
+
+        [TestMethod]
+        public void ShouldUseKeySuppliedInConstructor() {
+            var newTarget = new CaesarShiftDecryptionAlgorithm(16);
+            var encryptedSentence = "AesMu LbYgx PyH TeWZc yFob dro VkJI Nyq";
+            var decryptedSentence = "QuiCk BrOwn FoX JuMPs oVer the LaZY Dog";
+
+            Assert.AreEqual(decryptedSentence, newTarget.DecryptText(encryptedSentence));
+        }
+    }
+}
