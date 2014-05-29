168e5872b204ccf82ca1e842bcace97d26426fff
Eoin Mullan <eoinmullan@hotmail.com>
2014-05-29 23:57:47 +0100

    Removing redundant enum to Boolean converter.

--- C#/Decryption/MainWindow.xaml.cs
+++ C#/Decryption/MainWindow.xaml.cs
@@ -30,14 +30,4 @@ namespace Decryption {
             DataContext = new DecrypterViewModel(caesarShiftAlgorithm, xorAlgorithm, monoAlphaAlgorithm);
         }
     }
-
-    public class EnumToBooleanConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-            return value.Equals(parameter);
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
-            return value.Equals(true) ? parameter : Binding.DoNothing;
-        }
-    }
 }
