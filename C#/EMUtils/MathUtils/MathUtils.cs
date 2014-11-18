88db4316db311ca89333ec4f379694e50c48a2e3
Eoin Mullan <eoinmullan@hotmail.com>
2014-11-18 22:10:38 +0000

    Adding MathUtils solution

--- C#/EMUtils/MathUtils/MathUtils.cs
+++ C#/EMUtils/MathUtils/MathUtils.cs
@@ -4,34 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EMUtils
-{
-    static public class MathUtils
-    {
-        public static List<bool> GetPrimesBoolArrayToN(int n)
-        {
+namespace EMUtils {
+    static public class MathUtils {
+
+        public static List<bool> GetPrimesBoolArrayToN(int n) {
 	        if (n < 0) {
 		        throw new ArgumentOutOfRangeException("Cannot get primes between zero and a negative number");
             }
 
             // Determine primes using Sieve of Eratosthenes
             var primeArray = new List<bool>(n + 1);
-            for (var i = 0; i <= n; i++)
-            {
+            for (var i = 0; i <= n; i++) {
                 primeArray.Add(true);
             }
+
             primeArray[0] = false;
-            if (n > 0)
-            {
+            if (n > 0) {
                 primeArray[1] = false;
                 int i = 2;
-                while (i < n)
-                {
-                    if (primeArray[i])
-                    {
+                while (i < n) {
+                    if (primeArray[i]) {
                         int j = i + i;
-                        while (j <= n)
-                        {
+                        while (j <= n) {
                             primeArray[j] = false;
                             j += i;
                         }
@@ -43,8 +37,7 @@ namespace EMUtils
             return primeArray;
         }
 
-        public static List<int> GetPrimesToN(int n)
-        {
+        public static List<int> GetPrimesToN(int n) {
             var nums = GetPrimesBoolArrayToN(n);
             var primes = new List<int>();
 
