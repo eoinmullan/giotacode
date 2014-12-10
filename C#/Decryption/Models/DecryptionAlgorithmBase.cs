d4976b98c52e774e69c68c72da2c788772aa7721
Eoin Mullan <eoinmullan@hotmail.com>
2014-12-10 20:15:04 +0000

    Missed from last check-in

---
+++ C#/Decryption/Models/DecryptionAlgorithmBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Decryption.Interfaces;
+
+namespace Decryption.Models {
+    internal abstract class DecryptionAlgorithmBase : IDecryptionAlgorithm {
+        public event EventHandler EncryptionChanged;
+
+        public abstract string DecryptText(string encryptedText);
+
+        protected void RaiseEncryptionChanged() {
+            if (EncryptionChanged != null) {
+                EncryptionChanged(this, new EventArgs());
+            }
+        }
+    }
+}
