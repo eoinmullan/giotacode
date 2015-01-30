4b9dadc94f135cf6e951f630222d1a8dc8691611
Eoin Mullan <eoinmullan@hotmail.com>
2015-01-30 22:56:36 +0000

    Adding a design time view model for the main window.

---
+++ C#/Decryption/ViewModels/MockDecrypterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Decryption.Interfaces;
+using Decryption.Models;
+
+namespace Decryption.ViewModels {
+    internal class MockDecrypterViewModel {
+        private List<IDecrypter> decrypters;
+        public List<IDecrypter> Decrypters {
+            get {
+                return decrypters;
+            }
+        }
+
+        public MockDecrypterViewModel() {
+            decrypters = new List<IDecrypter>() {
+                new CaesarShiftDecrypter(),
+                new XORDecrypter(null, null, null)
+            };
+        }
+    }
+}
